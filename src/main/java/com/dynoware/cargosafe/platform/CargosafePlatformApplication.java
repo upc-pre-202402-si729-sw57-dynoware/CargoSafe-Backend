c26c3c640673ac18a1a4957b2954cf12017a25a6
Alumnos <alumnos@H51-11.local>
2024-10-16 15:40:35 -0500

    chore: project setup

---
+++ src/main/java/com/dynoware/cargosafe/platform/CargosafePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.dynoware.cargosafe.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargosafePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CargosafePlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dynoware/cargosafe/platform/CargosafePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dynoware.cargosafe.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargosafePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
