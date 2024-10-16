08d1355c575a89c14c123dc27da3b80070a4a662
rivacortez <rivaldo062134@gmail.com>
2024-10-16 16:28:27 -0500

    feat(orm): setup persistence physical naming strategy.

--- src/main/java/com/dynoware/cargosafe/platform/shared/domain/model/entities/AuditableModel.java
+++ src/main/java/com/dynoware/cargosafe/platform/shared/domain/model/entities/AuditableModel.java
@@ -1,27 +1,25 @@
-package com.dynoware.cargosafe.platform.shared.domain.model.aggregates;
+package com.dynoware.cargosafe.platform.shared.domain.model.entities;
 
-
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.domain.AbstractAggregateRoot;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
-@Getter
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
-public class AuditableAbstractAggregateRoot<T extends AbstractAggregateRoot<T>> extends AbstractAggregateRoot<T> {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class AuditableModel {
 
+    @Getter
     @CreatedDate
     @Column(nullable = false, updatable = false)
     private Date createdAt;
 
+    @Getter
     @LastModifiedDate
     @Column(nullable = false)
     private Date updatedAt;
