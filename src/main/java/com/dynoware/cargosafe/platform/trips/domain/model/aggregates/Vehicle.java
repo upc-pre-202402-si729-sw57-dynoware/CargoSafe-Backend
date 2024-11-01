ad3d91e51f7c7b7ae00196d3e846f403ccfe5eeb
rrenzosilva <149326018+rrenzosilva@users.noreply.github.com>
2024-10-31 20:24:52 -0500

    feat(vehicles):  add aggregate create, delete and update vehicle command handling at root level

---
+++ src/main/java/com/dynoware/cargosafe/platform/trips/domain/model/aggregates/Vehicle.java
@@ -0,0 +1,43 @@
+package com.dynoware.cargosafe.platform.trips.domain.model.aggregates;
+
+import com.dynoware.cargosafe.platform.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import com.dynoware.cargosafe.platform.trips.domain.model.commands.CreateVehicleCommand;
+import com.dynoware.cargosafe.platform.trips.domain.model.commands.UpdateVehicleCommand;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "vehicles")
+public class Vehicle extends AuditableAbstractAggregateRoot<Vehicle> {
+    private Long id;
+    private String model;
+    private String plate;
+    private float max_load;
+    private float volume;
+    private String photo_url;
+
+    public Vehicle(CreateVehicleCommand command){
+        this.model = command.model();
+        this.plate = command.plate();
+        this.max_load = command.max_load();
+        this.volume = command.volume();
+        this.photo_url = command.photo_url();
+    }
+
+    public Vehicle updateVehicle(UpdateVehicleCommand command) {
+        this.model = command.model();
+        this.plate = command.plate();
+        this.max_load = command.max_load();
+        this.volume = command.volume();
+        this.photo_url = command.photo_url();
+        return this;
+    }
+}
---
+++ src/main/java/com/dynoware/cargosafe/platform/trips/domain/model/commands/DeleteVehicleCommand.java
@@ -0,0 +1,4 @@
+package com.dynoware.cargosafe.platform.trips.domain.model.commands;
+
+public record DeleteVehicleCommand(Long id) {
+}
