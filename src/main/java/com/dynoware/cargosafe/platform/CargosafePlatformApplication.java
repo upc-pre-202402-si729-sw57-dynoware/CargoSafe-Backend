9c8934957ed4d28e7ac24cf732c2a9ce6f9dd621
rrenzosilva <149326018+rrenzosilva@users.noreply.github.com>
2024-11-01 21:56:15 -0500

    feat: add dependencies

--- src/main/java/com/dynoware/cargosafe/platform/CargosafePlatformApplication.java
+++ src/main/java/com/dynoware/cargosafe/platform/CargosafePlatformApplication.java
@@ -2,7 +2,9 @@ package com.dynoware.cargosafe.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CargosafePlatformApplication {
 
