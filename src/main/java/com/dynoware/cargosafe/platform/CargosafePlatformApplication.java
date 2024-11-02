c2ed365d6df9c614c4aa32ddbc9f6e04e6455570
Alexandra Ramos <149441307+AleRamosA@users.noreply.github.com>
2024-11-02 18:10:53 -0500

    feat: add dependencies.

--- src/main/java/com/dynoware/cargosafe/platform/CargosafePlatformApplication.java
+++ src/main/java/com/dynoware/cargosafe/platform/CargosafePlatformApplication.java
@@ -2,8 +2,11 @@ package com.dynoware.cargosafe.platform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
+
 public class CargosafePlatformApplication {
 
     public static void main(String[] args) {
