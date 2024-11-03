76a8d132cf80eca1de0bf26dd5ec7276cfdd96ab
rivacortez <rivaldo062134@gmail.com>
2024-11-03 15:09:58 -0500

    feat: add ProfileQueryService interface with query handling methods

---
+++ src/main/java/com/dynoware/cargosafe/platform/profiles/domain/model/commands/CreateProfileCommand.java
@@ -0,0 +1,14 @@
+package com.dynoware.cargosafe.platform.profiles.domain.model.commands;
+
+/**
+ * Create Profile Command
+ */
+public record CreateProfileCommand(String firstName,
+                                   String lastName,
+                                   String email,
+                                   String street,
+                                   String number,
+                                   String city,
+                                   String postalCode,
+                                   String country) {
+}
---
+++ src/main/java/com/dynoware/cargosafe/platform/profiles/domain/model/queries/GetAllProfilesQuery.java
@@ -0,0 +1,7 @@
+package com.dynoware.cargosafe.platform.profiles.domain.model.queries;
+
+/**
+ * Get All Profiles Query
+ */
+public record GetAllProfilesQuery() {
+}
