4d9614cb7fddd53a8564725ddc83178994036aaa
rivacortez <rivaldo062134@gmail.com>
2024-11-18 15:32:07 -0500

    feat(trip): implement create trip resource & trip resource

---
+++ src/main/java/com/dynoware/cargosafe/platform/trips/interfaces/rest/resources/TripResource.java
@@ -0,0 +1,29 @@
+package com.dynoware.cargosafe.platform.trips.interfaces.rest.resources;
+
+public record TripResource(
+        Long id,
+        String name,
+        String type,
+        double weight,
+        String unloadDirection,
+        String unloadLocation,
+        String unloadDate,
+        Long expenseId,
+        Long alertId,
+        Long ongoingTripId,
+        Long vehicleId,
+        Long driverId,
+        Long userId,
+        String destination,
+        String department,
+        String district,
+        String country,
+        int numberPackages,
+        String holderName,
+        String destinationDate,
+        double totalAmount,
+        String destinationAddress,
+        String loadDetail,
+        String pickupAddress
+) {
+}
